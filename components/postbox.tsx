b81c63c300ded8055633ce0ce944a5730e7c471c
Camden Mecklem <c@camdenmecklem.com>
2021-10-06 18:13:35 -0400

    use nextjs image optimization for postbox backgrounds

--- components/postbox.tsx
+++ components/postbox.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Date from '../components/date'
 import styles from '../styles/PostBox.module.css'
+import Image from 'next/image'
 
 export enum boxSize {
   small,
@@ -23,13 +24,14 @@ const PostBox: React.FC<Props> = ({ postData, size }) => {
     <Link key={postData.id} href={`/blog/${postData.id}`}>
       <a className={styles.blogLink} tabIndex={-1}>
         <div className={size === boxSize.large ? `postbox ${styles.largePostbox}` : `postbox ${styles.smallPostbox}`} tabIndex={0}>
-          <style jsx>
-            {
-              `.postbox {
-                background-image: url(${postData.imageURL})
-              }`
-            }
-          </style>
+          <Image
+            className={styles.backgroundImage}
+            src={postData.imageURL}
+            alt=''
+            layout='fill'
+            objectFit='cover'
+            objectPosition='center'
+          />
           <div className={styles.postboxContent}>
             <p>
                 {postData.title}
